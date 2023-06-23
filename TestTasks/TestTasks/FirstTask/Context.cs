0dde790d344c6dda8eb7fd2fb1acf34df95e9f35
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-06-23 21:45:33 +0400

    AddingTestTasks

---
+++ TestTasks/TestTasks/FirstTask/Context.cs
@@ -0,0 +1,20 @@
+﻿using DataBaseModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Context;
+
+internal class Context : DbContext
+{
+    public DbSet<Device> Devices { get; set; }
+    public DbSet<Mode> Modes { get; set; }
+ 
+    public Context()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Tickets;Trusted_Connection=True;");
+    }
+}
---
+++ TestTasks/TestTasks/SecondTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using CommandProcessingModel;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        List<CommandHandler> _handlers = new List<CommandHandler>
+        {
+            new Command1Handler(),
+            new Command2Handler()
+        };
+
+        List<string> commands = new List<string>
+        {
+            "Command1;hello",
+            "Command2;beatboks",
+            "Command1;TeamLeadLoh!"
+        };
+
+        CommandHandlersController commandHandlersController = new CommandHandlersController(_handlers);
+
+        foreach (var command in commands)
+            await commandHandlersController.StartHendlAsync(command.Split(';')[0], command.Split(';')[1]);
+    }
+}
